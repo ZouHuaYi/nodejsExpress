e2f3d6eb125f9b08e93886ba9b2e750e6e8134e5
zhy <1004753639@qq.com>
2019-01-31 18:03:45 +0800

    1-31

--- routes/insert.js
+++ routes/insert.js
@@ -7,32 +7,28 @@ var randomStr = require('../utils/random');
 var insert = new InsertTest();
 
 router.get('/', function (req, res, next) {
-    insertNum(10000);
     res.send('好海哦');
 })
 
 function insertNum(num) {
-
-    for (var i = 0; i < num; i++) {
-        var data = {
-            name: randomStr(true, 5, 20),
-            city: city[randomNumber(0, 33)]['name'],
-            status: randomNumber(0, 4),
-            price: randomNumber(1, 900000),
-            count: randomNumber(1, 50000)
-        }
-
-        insert.insert(data, function (err, result) {
-            if (err) {
-                console.log("出错了");
-
-            } else {
-
-            }
-
-        })
+    if (num <= 0) return;
+    var data = {
+        name: randomStr(true, 5, 20),
+        city: city[randomNumber(0, 33)]['name'],
+        status: randomNumber(0, 4),
+        price: randomNumber(1, 900000),
+        count: randomNumber(1, 50000)
     }
 
+    insert.insert({ ...data }, function (err, result) {
+        if (err) {
+            console.log("出错了");
+        } else {
+            num--;
+            insertNum(num)
+        }
+    })
 }
+
 
 module.exports = router;
--- utils/dataQuery.js
+++ utils/dataQuery.js
@@ -3,7 +3,7 @@
  * @Author: zouhuayi
  * @Date: 2019-01-23 17:46:58
  * @LastEditors: zouhuayi
- * @LastEditTime: 2019-01-30 23:44:12
+ * @LastEditTime: 2019-01-31 10:23:07
  */
 
 var db = require('../database');
@@ -14,10 +14,10 @@ var query = function (sql, data, callback) {
     var sql = formatSql(sql, data);
     db.pool.getConnection(function (err, connection) {
         if (err) {
+            console.log("无法连接");
             callback(true, '无法连接');
             return;
         }
-        // make the query
         connection.query(sql, data, function (err, results) {
             if (err) {
                 callback(true);
@@ -25,7 +25,7 @@ var query = function (sql, data, callback) {
             }
             callback(false, results);
         });
-           connection.release(); // 释放 连接放回连接池，等待其它使用者使用
+        connection.release(); // 释放 连接放回连接池，等待其它使用者使用
     });
 }
 
